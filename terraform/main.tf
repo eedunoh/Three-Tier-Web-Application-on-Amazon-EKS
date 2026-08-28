7e933a8cada325913f8214808adaee65fe654de4
eedunoh <63975169+eedunoh@users.noreply.github.com>
2026-08-28 02:00:13 +0100

    minor update to add k8 provider to help with creating eks web app pod service accounts

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,7 +46,7 @@ provider "helm" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.main.endpoint
+  host                   = aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.main.token
 }
